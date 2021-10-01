da96094fed27ebb234bac17d4c106c2bed7719e4
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-10-02 01:25:17 +0400

    feat: Added custom app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+// import "../styles/main.scss";
+import React from "react";
+import NextNprogress from "nextjs-progressbar";
+import PropTypes from "prop-types";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextNprogress
+        color="#29D"
+        startPosition="0.3"
+        stopDelayMs="200"
+        height="3"
+      />
+
+      <Component {...pageProps} />
+
+      <NextNProgress options={{ easing: "ease", speed: 500 }} />
+
+      <style jsx global>{`
+        body {
+          font-family: "Roboto", sans-serif;
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default MyApp
+NextNProgress.propTypes = {
+  color: PropTypes.string,
+  startPosition: PropTypes.number,
+  stopDelayMs: PropTypes.number,
+  options: PropTypes.object,
+};
