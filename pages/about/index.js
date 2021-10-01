e515516e38c5addb89b64c777634c135a2f7573a
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-10-02 02:00:42 +0400

    fix: error

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,7 +3,7 @@ import Router from "next/router";
 import React from "react";
 import MainLayout from "../../components/MainLayout";
 
-export default function About() {
+export default function About({ title }) {
   const handleClick = () => {
     Router.push("/");
     // console.log("ROUTER", Router);
@@ -11,7 +11,7 @@ export default function About() {
 
   return (
     <MainLayout title={"About Page"}>
-      <h1>About Page</h1>
+      <h1>{title}</h1>
       <button onClick={handleClick}> Go To Home Page</button>
       <button onClick={() => Router.push("/posts")}> Go To Post Page</button>
     </MainLayout>
