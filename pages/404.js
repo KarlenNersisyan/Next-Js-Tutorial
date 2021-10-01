f310ee20582dc74515b12bf7aae8bd0293bb42e9
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-10-02 01:24:56 +0400

    feat: Added not fount

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import MainLayout from "../components/MainLayout";
+
+import classes from "../styles/error.module.scss";
+
+export default function ErrorPage() {
+  return (
+    <MainLayout>
+      <h1 className={classes.error}> Error 404 | Not Fount</h1>
+      <p>
+        Please
+        <Link href={"/"}>
+          <a> go back </a>
+        </Link>
+        to safety.
+      </p>
+    </MainLayout>
+  );
+}
